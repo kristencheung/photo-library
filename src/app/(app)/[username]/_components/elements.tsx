78691c5c3fabfffed3b08a72992bb7c4f1892fb7
kristencheung <krcheung9@gmail.com>
2024-05-14 08:29:09 -0700

    add elements

--- src/app/(app)/[username]/_components/elements.tsx
+++ src/app/(app)/[username]/_components/elements.tsx
@@ -14,7 +14,7 @@ export const Elements = async () => {
     <div className="gap-[60px] mt-14 grid grid-cols-1 sm:grid-cols-[repeat(2,minmax(0px,1fr))] md:grid-cols-[repeat(3,minmax(0px,1fr))] lg:grid-cols-[repeat(4,minmax(0px,1fr))]">
       {elements && elements?.length > 0 ? (
         elements?.map((element) => {
-          return <Element element={element} />
+          return <Element key={element?.image} element={element} />
         })
       ) : (
         <div>No elements found</div>
