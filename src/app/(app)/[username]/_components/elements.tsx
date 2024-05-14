9fe69b55d8c66810fe7437ec79c0d71fd6d335f2
kristencheung <krcheung9@gmail.com>
2024-05-14 09:48:08 -0700

    add self start

--- src/app/(app)/[username]/_components/elements.tsx
+++ src/app/(app)/[username]/_components/elements.tsx
@@ -11,7 +11,7 @@ export const Elements = async () => {
   const elements = await getElementsByUserId({ userId: user?.id! })
 
   return (
-    <div className="pb-[80px] justify-center items-center gap-[60px] mt-14 grid grid-cols-1 sm:grid-cols-[repeat(2,minmax(0px,1fr))] md:grid-cols-[repeat(3,minmax(0px,1fr))] lg:grid-cols-[repeat(4,minmax(0px,1fr))]">
+    <div className="pb-[80px] self-start gap-[60px] mt-14 grid grid-cols-1 sm:grid-cols-[repeat(2,minmax(0px,1fr))] md:grid-cols-[repeat(3,minmax(0px,1fr))] lg:grid-cols-[repeat(4,minmax(0px,1fr))]">
       {elements && elements?.length > 0 ? (
         elements?.map((element) => {
           return <Element key={element?.image} element={element} />
